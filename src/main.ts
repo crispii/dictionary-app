13a4e140841a961b3fbb8749795bf7a222914f93
crispii <jeewoo.yoon@gmail.com>
2026-03-12 03:28:36 -0400

    dictionary-03: Use interfaces for type declarations

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,30 @@
 import "./style.css";
 
-type Definition = {
+interface Definition {
   definition: string;
   example?: string;
   synonyms?: string[];
   antonyms?: string[];
-};
+}
 
-type Meaning = {
+interface Meaning {
   partOfSpeech: string;
   definitions: Definition[];
-};
+}
 
-type Phonetic = {
+interface Phonetic {
   text?: string;
   audio?: string;
-};
+}
 
-type DictionaryAPIResponse = {
+interface DictionaryAPIResponse {
   word: string;
   phonetic?: string;
   phonetics: Phonetic[];
   origin?: string;
   meanings: Meaning[];
-};
+}
+
 
 const dictionaryAPI = "https://api.dictionaryapi.dev/api/v2/entries/en_US/";
 
